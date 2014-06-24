53fb1425e96773f416704b6a992008daa2241007
Jon Hancock <jhancock@shellshadow.com>
2014-06-24 15:53:35 +0800

    added initial tags table and listing by tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,17 @@
 module ApplicationHelper
 
+    @@tags_table_all = nil
+    def tags_table_all
+      return @@tags_table_all if @@tags_table_all
+      tags_table = "<div><b>内容标签</b><table cellpadding='0px' cellspacing='0px' class='tags'>"
+      GenreTag.grouped_by_3.each do |subarray|
+        tags_table << "<tr>"
+        subarray.each do |tag|
+          tags_table << "<td><a href='#{books_by_tag_path(tag: tag.name)}'>#{tag.cn}</a></td>"
+        end
+        tags_table << "</tr>"
+      end
+      @@tags_table_all = tags_table << "</table></div>"
+    end
+
 end
