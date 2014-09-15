b2d54f121ea6b2071fbbe70b794eef2258700f8d
Jon Hancock <jhancock@shellshadow.com>
2014-09-15 09:28:35 +0000

    add some basic indexes to users and books

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,50 +1,57 @@
 class Book
-	include Mongoid::Document
-	include Mongoid::Timestamps
+  include Mongoid::Document
+  include Mongoid::Timestamps
 
-	field :title, type: String
-	field :author, type: String
-	field :summary, type: String
-	field :summary_html, type: String
-	field :tags, type: Array, default: []
-	field :tag_names, type: Array, default: []
-	field :tag_names_pp, type: String
+  field :title, type: String
+  field :author, type: String
+  field :summary, type: String
+  field :summary_html, type: String
+  field :tags, type: Array, default: []
+  field :tag_names, type: Array, default: []
+  field :tag_names_pp, type: String
 
-	field :origin, type: String
-	field :type, type: String
-	field :chars, type: Integer
-	field :chunks, type: Integer
-	field :read_count, type: Integer, default: 0
+  field :origin, type: String
+  field :type, type: String
+  field :chars, type: Integer
+  field :chunks, type: Integer
+  field :read_count, type: Integer, default: 0
 
-	# should default to nil?  or change this to a state field?
-	field :offline_at, type: Time
-	field :offline_reason, type: String
+  # should default to nil?  or change this to a state field?
+  field :offline_at, type: Time
+  field :offline_reason, type: String
 
-	def self.online_recent
-		self.online_criteria.desc(:updated_at)
-	end
+  index({ title: 1, author: 1, offline_at: 1 }, { unique: false })
+  index title: 1
+  index author: 1
+  index offline_at: 1
+  index updated_at: -1
+  index read_count: -1
 
-	def self.online_popular
-		self.online_criteria.desc(:read_count)
-	end
+  def self.online_recent
+    self.online_criteria.desc(:updated_at)
+  end
 
-	def self.online_recent_by_tag(tag)
-		self.online_recent.in(tags: tag.id)
-	end
+  def self.online_popular
+    self.online_criteria.desc(:read_count)
+  end
 
-	def self.online_popular_by_tag(tag)
-		self.online_popular.in(tags: tag.id)
-	end
+  def self.online_recent_by_tag(tag)
+    self.online_recent.in(tags: tag.id)
+  end
 
-	def self.online_criteria
-		criteria = Book.exists(title: true, author: true, offline_at: false)
-		if Rails.configuration.mihudie.suppress_tags
-			criteria = criteria.nin(tags: Rails.configuration.mihudie.suppress_tags)
-		end
-		criteria
-	end
+  def self.online_popular_by_tag(tag)
+    self.online_popular.in(tags: tag.id)
+  end
 
-	def chunk_path(chunk_id)
-    	"#{Rails.configuration.mihudie.books_path_prefix}#{self.id}/formatted/#{chunk_id}.html"
-  	end
+  def self.online_criteria
+    criteria = Book.exists(title: true, author: true, offline_at: false)
+    if Rails.configuration.mihudie.suppress_tags
+      criteria = criteria.nin(tags: Rails.configuration.mihudie.suppress_tags)
+    end
+    criteria
+  end
+
+  def chunk_path(chunk_id)
+    "#{Rails.configuration.mihudie.books_path_prefix}#{self.id}/formatted/#{chunk_id}.html"
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,66 +1,69 @@
 class User
-	include Mongoid::Document
-	include Mongoid::Timestamps
+  include Mongoid::Document
+  include Mongoid::Timestamps
 
-	# TODO add index on email and probably many others ;)
-  	field :email, type: String
-  	# password_hash and password_salt are old hash method.
-  	field :password_hash, type: String
-  	field :password_salt, type: String
-  	# TODO add field and upgrade method for getting password hashes in bcrypt
-  	#field :password_bcrypt, type: String
+  # TODO add index on email and probably many others ;)
+  field :email, type: String
+  # password_hash and password_salt are old hash method.
+  field :password_hash, type: String
+  field :password_salt, type: String
+  # TODO add field and upgrade method for getting password hashes in bcrypt
+  #field :password_bcrypt, type: String
 
-  	field :password_reset_at, type: Time
-  	field :password_reset_code, type: String  	
+  field :password_reset_at, type: Time
+  field :password_reset_code, type: String  	
 
-  	field :registered, type: Time
-	field :premium, type: Time
-	field :admin, type: Time
+  field :registered, type: Time
+  field :premium, type: Time
+  field :admin, type: Time
 
-	field :geo, type: Array
-	field :cn, type: String  # country code from the geo array
+  field :geo, type: Array
+  field :cn, type: String  # country name (two char code) from the geo array
 
-	# only used twice in old system for testing
-	field :referral_code, type: String
-	# only used once on jhancock@shellshadow.com account
-	field :referred_by, type: String
+  # only used twice in old system for testing
+  field :referral_code, type: String
+  # only used once on jhancock@shellshadow.com account
+  field :referred_by, type: String
 
-	# intended for arbitrary error info for debugging purposes 
-	# and to ensure we don't try to send to bad addresses more than once
-	field :email_error, type: String
+  # intended for arbitrary error info for debugging purposes 
+  # and to ensure we don't try to send to bad addresses more than once
+  field :email_error, type: String
 
-	# returns an authenticated user or nil
-	def self.authenticate(email, password)
-		user = self.find_by(email: email)
-		return user if user && verify_password?(password, user.password_hash, user.password_salt)
-		nil
-	end
+  index email: 1
+  index cn: 1
 
-	# returns true if the password is correct
-	def self.verify_password?(password, password_hash, password_salt)
-		password_hash == OpenSSL::Digest::SHA1.hexdigest("#{password_salt}#{password}")    
-	end
+  # returns an authenticated user or nil
+  def self.authenticate(email, password)
+    user = self.find_by(email: email)
+    return user if user && verify_password?(password, user.password_hash, user.password_salt)
+    nil
+  end
 
-	def set_password_hash(password)
-		# create a password_salt if it doesn't exist
-		self.password_salt = create_password_salt
-		self.password_hash = OpenSSL::Digest::SHA1.hexdigest("#{self.password_salt}#{password}")
-	end
+  # returns true if the password is correct
+  def self.verify_password?(password, password_hash, password_salt)
+    password_hash == OpenSSL::Digest::SHA1.hexdigest("#{password_salt}#{password}") 
+  end
 
-	private
-	def create_password_salt
-		self.class.generate_code(16)
-	end
+  def set_password_hash(password)
+    # create a password_salt if it doesn't exist
+    self.password_salt = create_password_salt
+    self.password_hash = OpenSSL::Digest::SHA1.hexdigest("#{self.password_salt}#{password}")
+  end
 
-	def self.generate_code(length)
-		# return a string of <length> digits in base 30 format according to BASE_30_ARRAY.
-		id = ''
-		length.times do
-			id << BASE_30_ARRAY[rand(30)]
-		end
-		return id
-	end
+  private
+  def create_password_salt
+    self.class.generate_code(16)
+  end
+
+  def self.generate_code(length)
+    # return a string of <length> digits in base 30 format according to BASE_30_ARRAY
+    id = ''
+    length.times do
+      id << BASE_30_ARRAY[rand(30)]
+    end
+    return id
+  end
     
-	BASE_30_ARRAY = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']  
+  BASE_30_ARRAY = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']  
   
 end
