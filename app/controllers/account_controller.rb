f8e6b4b879a9f2dd2bd9359b12e1ea363d8491a2
Jon Hancock <jhancock@shellshadow.com>
2014-11-08 15:58:11 +0000

    refactored collecion rendering

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -4,7 +4,7 @@ class AccountController < ApplicationController
   before_action :ensure_user, except: :logout
 
   def index
-    @page_title = "账户管理"
+    @page_title = "Account"
     @page_description = "在线管理自己账户的密码和最近阅读的书签信息 "
     @page_keywords = "账户管理 书签 电子书"
   end
